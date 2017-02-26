590e2990fa26edae3baf5b8f59f898f322e8ae49
Unknown <omaremadsabry@gmail.com>
2017-02-26 21:34:35 +0200

    [+] Added Merperson class
    added constructor and getter method , This class is a subclass of Obstacle

---
+++ src/harrypotter/model/world/Merperson.java
@@ -0,0 +1,16 @@
+package harrypotter.model.world;
+
+public class Merperson extends Obstacle{
+	private int damage ;
+	
+	public Merperson(int hp , int damage)
+	{
+		super(hp);
+		this.damage = damage ;
+	}
+	public int getDamage()
+	{
+		return damage ;
+	}
+
+}
