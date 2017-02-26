1f7e5728baaaf6a29b179f78a44cce0b3a531c13
Unknown <omaremadsabry@gmail.com>
2017-02-26 22:14:44 +0200

    [+] Added ObstacleCell subclass
    Class with constructor and getter method

---
+++ src/harrypotter/model/world/ObstacleCell.java
@@ -0,0 +1,15 @@
+package harrypotter.model.world;
+
+public class ObstacleCell extends Cell{
+	private Obstacle obstacle ;
+	
+	public ObstacleCell(Obstacle obstacle)
+	{
+		this.obstacle = obstacle ;
+	}
+	public Obstacle getObstacle()
+	{
+		return obstacle ;
+	}
+
+}
