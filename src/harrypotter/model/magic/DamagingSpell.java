34f77eda5e0d9e7066b7fe8fbdc2f006a8ebd63f
GPhantom <shadytantawy@protonmail.com>
2017-02-26 21:32:46 +0200

    [+] Added DamagingSpell Class
    -A sub class from Spell super class.

---
+++ src/harrypotter/model/magic/DamagingSpell.java
@@ -0,0 +1,17 @@
+package harrypotter.model.magic;
+
+public class DamagingSpell extends Spell{
+	private int damageAmount;
+	
+	public DamagingSpell(String name, int cost, int defaultCoolDown, int damageAmount){
+		super(name, cost, defaultCoolDown);
+		this.damageAmount = damageAmount;
+	}
+
+	public int getDamageAmount() {
+		return damageAmount;
+	}
+	
+	
+
+}
