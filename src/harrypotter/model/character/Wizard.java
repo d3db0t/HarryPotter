0520767676c96b1727cdbdd8fa73861521e7b561
GPhantom <shadytantawy@protonmail.com>
2017-02-26 22:36:47 +0200

    [+] Added Wizard Super Class
    -A super class that have 4 sub classes.

---
+++ src/harrypotter/model/character/Wizard.java
@@ -0,0 +1,88 @@
+package harrypotter.model.character;
+
+import java.awt.Point;
+import java.util.ArrayList;
+
+import harrypotter.model.magic.Spell;
+
+public abstract class Wizard {
+	private String name;
+	private int defaultHp;
+	private int defaultIp;
+	private int hp;
+	private int ip;
+	private ArrayList<Spell> spells;
+	private ArrayList<Collectible> inventory;
+	private Point location;
+	private int traitCooldown;
+	
+	public Wizard(String name){
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getDefaultHp() {
+		return defaultHp;
+	}
+
+	public void setDefaultHp(int defaultHp) {
+		this.defaultHp = defaultHp;
+	}
+
+	public int getDefaultIp() {
+		return defaultIp;
+	}
+
+	public void setDefaultIp(int defaultIp) {
+		this.defaultIp = defaultIp;
+	}
+
+	public int getHp() {
+		return hp;
+	}
+
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+
+	public int getIp() {
+		return ip;
+	}
+
+	public void setIp(int ip) {
+		this.ip = ip;
+	}
+
+	public Point getLocation() {
+		return location;
+	}
+
+	public void setLocation(Point location) {
+		this.location = location;
+	}
+
+	public int getTraitCooldown() {
+		return traitCooldown;
+	}
+
+	public void setTraitCooldown(int traitCooldown) {
+		this.traitCooldown = traitCooldown;
+	}
+
+	public ArrayList<Spell> getSpells() {
+		return spells;
+	}
+
+	public ArrayList<Collectible> getInventory() {
+		return inventory;
+	}
+	
+	
+}
