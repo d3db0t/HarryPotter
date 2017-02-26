6c421fe269eec8d68dac77915ab5295191ef8853
GPhantom <shadytantawy@protonmail.com>
2017-02-26 23:21:41 +0200

    [+] Added SlytherinWizard subclass
    -A subclass from Wizard superclass.

---
+++ src/harrypotter/model/character/SlytherinWizard.java
@@ -0,0 +1,15 @@
+package harrypotter.model.character;
+
+import harrypotter.model.world.Direction;
+
+public class SlytherinWizard extends Wizard implements Champion{
+	Direction traitDirection;
+	
+	public SlytherinWizard(String name, Direction traitDirection){
+		super(name);
+		super.setDefaultHp(850);
+		super.setDefaultIp(550);
+		this.traitDirection = traitDirection;
+	}
+
+}
