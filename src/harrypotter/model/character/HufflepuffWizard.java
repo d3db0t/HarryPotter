dbd312c1e6df2a09ca2f5508d712d8caebe97336
GPhantom <shadytantawy@protonmail.com>
2017-02-26 23:14:59 +0200

    [+] Added HufflepuffWizard subclass
    -A subclass from Wizard superclass.

---
+++ src/harrypotter/model/character/HufflepuffWizard.java
@@ -0,0 +1,11 @@
+package harrypotter.model.character;
+
+public class HufflepuffWizard extends Wizard implements Champion{
+	
+	public HufflepuffWizard(String name){
+		super(name);
+		super.setDefaultHp(1000);
+		super.setDefaultIp(450);
+	}
+
+}
