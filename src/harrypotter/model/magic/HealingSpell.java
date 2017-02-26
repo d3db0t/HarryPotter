a1c1a49df6d0b975cfec004ea9c7b2b9aa499bd4
GPhantom <shadytantawy@protonmail.com>
2017-02-26 21:25:07 +0200

    [+] Added HealingSpell Class
    - A sub class from Spell super Class.

---
+++ src/harrypotter/model/magic/HealingSpell.java
@@ -0,0 +1,17 @@
+package harrypotter.model.magic;
+
+public class HealingSpell extends Spell{
+	private int healingAmount;
+	
+	public HealingSpell(String name, int cost, int defaultCoolDown, int healingAmount){
+		super(name, cost, defaultCoolDown);
+		this.healingAmount = healingAmount;
+	}
+
+	public int getHealingAmount() {
+		return healingAmount;
+	}
+	
+	
+
+}
