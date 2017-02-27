61702cb9a561e69bc9b236626e670a11306f1026
GPhantom <shadytantawy@protonmail.com>
2017-02-27 20:53:53 +0200

    Added loadSpells() method
    -To read from csv file.
    -Location: Tournament Class.

--- src/harrypotter/model/tournament/Tournament.java
+++ src/harrypotter/model/tournament/Tournament.java
@@ -1,7 +1,12 @@
 package harrypotter.model.tournament;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.ArrayList;
 
+import harrypotter.model.magic.DamagingSpell;
+import harrypotter.model.magic.HealingSpell;
+import harrypotter.model.magic.RelocatingSpell;
 import harrypotter.model.magic.Spell;
 
 public class Tournament {
@@ -13,6 +18,7 @@ public class Tournament {
 	
 	public Tournament() throws Exception{
 		ArrayList<Champion> champions = new ArrayList<Champion>();
+		loadSpells("Spells.csv");
 		
 	}
 	
@@ -20,6 +26,37 @@ public class Tournament {
 		return Integer.parseInt(str);
 	}
 	
+	private void loadSpells(String filePath) throws Exception{
+		String currentLine = "";
+		FileReader fileReader= new FileReader(filePath);
+		BufferedReader br = new BufferedReader(fileReader);
+		while ((currentLine = br.readLine()) != null) {
+		
+			// Parsing the currentLine String
+			String [] result= currentLine.split(",");
+		
+			switch(result[0]){
+			case "DMG":
+				DamagingSpell d = new DamagingSpell(result[1], parseInt(result[2]),
+						parseInt(result[4]), parseInt(result[3]));
+				spells.add(d);
+				break;
+			case "HEL":
+				HealingSpell h = new HealingSpell(result[1], parseInt(result[2]),
+						parseInt(result[4]), parseInt(result[3]));
+				spells.add(h);
+				break;
+			case "REL":
+				RelocatingSpell r = new RelocatingSpell(result[1], parseInt(result[2]),
+						parseInt(result[4]), parseInt(result[3]));
+				spells.add(r);
+				break;
+			default:
+				break;
+			
+		}
+		}
+	}
 
 	public ArrayList<Champion> getChampions() {
 		return champions;
