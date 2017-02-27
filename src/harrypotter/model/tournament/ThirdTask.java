b6ec50f0b66d3b8ada3c3305fdd37dd7a929ac59
Unknown <omaremadsabry@gmail.com>
2017-02-27 23:41:20 +0200

    [+] Added constructor for ThirdTask
    readMap method still not implemented

--- src/harrypotter/model/tournament/ThirdTask.java
+++ src/harrypotter/model/tournament/ThirdTask.java
@@ -1,5 +1,18 @@
 package harrypotter.model.tournament;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import harrypotter.model.character.Champion;
+
 public class ThirdTask extends Task {
+	
+	public ThirdTask(ArrayList<Champion> champions)throws IOException
+	{
+		super(champions);
+		generateMap();
+	}
+	
 
 }
