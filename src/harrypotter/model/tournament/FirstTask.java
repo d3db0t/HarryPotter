a45fd7aeff05f1dcb5f59d9eb6cad50bec230899
Unknown <omaremadsabry@gmail.com>
2017-02-27 23:38:54 +0200

    [+] Added Constructor for FirstTask

--- src/harrypotter/model/tournament/FirstTask.java
+++ src/harrypotter/model/tournament/FirstTask.java
@@ -1,5 +1,17 @@
 package harrypotter.model.tournament;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import harrypotter.model.character.Champion;
+
 public class FirstTask extends Task{
+	
+	public FirstTask(ArrayList<Champion> champions)throws IOException
+	{
+		super(champions);
+		super.shuffleChampions();
+		generateMap();
+	}
 
 }
