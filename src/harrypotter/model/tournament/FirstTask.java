bcec296050c3571a918e48d958ba2ccd41f2a522
GPhantom <shadytantawy@protonmail.com>
2017-02-28 20:28:58 +0200

    Added methods to FirstTask Class
    -generateMap(): generates the map with 10 potions randomly .
    -addRandomPotions(): generates 10 potions randomly.
    -addPotionsToMap(): adds the 10 potions to the map randomly.
    -addObstacleCells(): generates 40 ObstacleCells Objects randomly and adds them to the map.

--- src/harrypotter/model/tournament/FirstTask.java
+++ src/harrypotter/model/tournament/FirstTask.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import harrypotter.model.character.Champion;
+import harrypotter.model.world.CollectibleCell;
+import harrypotter.model.world.ObstacleCell;
+import harrypotter.model.world.PhysicalObstacle;
 
 public class FirstTask extends Task{
 	
@@ -12,6 +15,52 @@ public class FirstTask extends Task{
 		super(champions);
 		super.shuffleChampions();
 		generateMap();
+	}
+	
+	@Override
+	public void generateMap()
+	{
+		super.generateMap();
+		addRandomPotions();
+		addObstacleCells();
+	}
+	@Override
+	public void addRandomPotions()
+	{
+		for(int i = 0 ;i < 10;i++)
+    	{
+    		int a = (int) Math.random()*6;
+    		addPotionsToMap(a);
+    	}
+	}
+	private void addPotionsToMap(int a)
+	{
+		int x = (int) Math.random()*10;
+    	int y = (int) Math.random()*10;
+    	
+    	if(x == 4 && y == 4)
+    		addPotionsToMap(a);
+    	else if(!super.isEmptyCell(super.getMap()[x][y]))
+    		addPotionsToMap(a);
+    	else
+    		super.getMap()[x][y] = new CollectibleCell(super.getPotions().get(a));
+	}
+	public void addObstacleCells()
+	{
+	   for(int i = 0; i < 40 ;i++)
+	   {
+		   int x = (int)Math.random()*10;
+		   int y = (int)Math.random()*10;
+		   if(x == 4 && y == 4)
+			   i--;
+		   else if(!super.isEmptyCell(super.getMap()[x][y]))
+			   i--;
+		   else
+ 		   {   
+			   int range =200 +(int) (Math.random()*300);
+			   super.getMap()[x][y] = new ObstacleCell(new PhysicalObstacle(range));
+		   }
+	   }
 	}
 
 }
