b11ff59bc12b6f110ea0733db1f49a571b4cf3d0
GPhantom <shadytantawy@protonmail.com>
2017-04-05 13:57:33 +0200

    Added OutOfRangeException class

---
+++ src/harrypotter/exceptions/OutOfRangeException.java
@@ -0,0 +1,14 @@
+package harrypotter.exceptions;
+
+public class OutOfRangeException extends InvalidActionException{
+	private int allowedRange;
+
+	public int getAllowedRange() {
+		return allowedRange;
+	}
+	
+	public OutOfRangeException(int allowedRange){
+		super("Range out of bound\nallowedRange: " + allowedRange);
+	}
+
+}
