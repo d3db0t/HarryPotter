36defadf6837c29496ba10d12c2cfd93b9ee9808
GPhantom <shadytantawy@protonmail.com>
2017-04-05 13:52:53 +0200

    Added NotEnoughIPException class

---
+++ src/harrypotter/exceptions/NotEnoughIPException.java
@@ -0,0 +1,19 @@
+package harrypotter.exceptions;
+
+public class NotEnoughIPException extends NotEnoughResourcesException{
+	private int requiredIP;
+	private int remainingIP;
+	
+	public int getRequiredIP() {
+		return requiredIP;
+	}
+	public int getRemainingIP() {
+		return remainingIP;
+	}
+	
+	public NotEnoughIPException(int requiredIP, int remainingIP){
+		super("Not enough ip\nrequiredIP: "+ requiredIP + "\nremainingIP: " + remainingIP);
+	}
+	
+
+}
