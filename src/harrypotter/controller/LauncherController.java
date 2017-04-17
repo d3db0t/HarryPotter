6a391d307ee2bbcc923a4bccb9ad4e9dfac0d938
GPhantom <shadytantawy@protonmail.com>
2017-04-17 17:40:33 +0200

    Removed comments in PreGameLauncher

--- src/harrypotter/controller/LauncherController.java
+++ src/harrypotter/controller/LauncherController.java
@@ -1,10 +1,7 @@
 package harrypotter.controller;
 
-import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
 import java.io.IOException;
 import java.util.ArrayList;
 
--- src/harrypotter/view/PreGameLauncher.java
+++ src/harrypotter/view/PreGameLauncher.java
@@ -10,16 +10,10 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class PreGameLauncher extends JPanel{
-	//JPanel btnSouthPanel;
+
 	public PreGameLauncher(){
 		setLayout(null); // To be able to change the locations of objects inside the panel
-		//super();
-		//getContentPane().setBackground(Color.BLACK);
-		//btnSouthPanel = new JPanel(new BorderLayout());
-		//add(btnSouthPanel);
 	}
-	
-	
 	
 	public void addButtons(JButton btn){
 		this.add(btn);
