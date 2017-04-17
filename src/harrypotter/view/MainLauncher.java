7ef419540d6e793d93b649f8f3d472149d51f959
GPhantom <shadytantawy@protonmail.com>
2017-04-17 17:37:58 +0200

    Removed comments in MainLauncher

--- src/harrypotter/view/MainLauncher.java
+++ src/harrypotter/view/MainLauncher.java
@@ -1,34 +1,21 @@
 package harrypotter.view;
 
-import java.awt.BorderLayout;
 import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-
-import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class MainLauncher extends JPanel{
-
-	//JPanel btnPanel ;
 	
 	public MainLauncher() throws IOException{
 		setLayout(null); // To be able to change the locations of objects inside the panel
-		//super();
-	    //btnPanel = new JPanel(new BorderLayout());
-	    //add(btnPanel);
 	}
 	
 	public void addButtons(ArrayList <JButton> btns)
-	{ // Play , Quit	
-		//btnPanel.add(btns.get(0),BorderLayout.NORTH);
-		//btnPanel.add(btns.get(1),BorderLayout.SOUTH);
+	{	
 		this.add(btns.get(0));
 		this.add(btns.get(1));
 	}
