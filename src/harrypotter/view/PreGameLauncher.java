0f691979a8bd04345178d6af4731ec82137e186d
GPhantom <shadytantawy@protonmail.com>
2017-04-17 17:06:26 +0200

    Changed PreGameLauncher to JPanel

--- src/harrypotter/view/PreGameLauncher.java
+++ src/harrypotter/view/PreGameLauncher.java
@@ -6,11 +6,12 @@ import java.awt.Color;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
-public class PreGameLauncher extends Launcher{
-	JPanel btnSouthPanel;
+public class PreGameLauncher extends JPanel{
+	//JPanel btnSouthPanel;
 	public PreGameLauncher(){
-		super();
-		getContentPane().setBackground(Color.BLACK);
+		setLayout(null); // To be able to change the locations of objects inside the panel
+		//super();
+		//getContentPane().setBackground(Color.BLACK);
 		//btnSouthPanel = new JPanel(new BorderLayout());
 		//add(btnSouthPanel);
 	}
