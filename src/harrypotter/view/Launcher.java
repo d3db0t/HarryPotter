182ee612e671d7fcec157b841d086f5092b8815f
GPhantom <shadytantawy@protonmail.com>
2017-04-17 16:59:43 +0200

    Added addPanel() method in Launcher
    This is the Frame.
    -Removed abstract so we can initialize a frame (Launcher).

--- src/harrypotter/view/Launcher.java
+++ src/harrypotter/view/Launcher.java
@@ -1,12 +1,18 @@
 package harrypotter.view;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public abstract class Launcher extends JFrame{
+public class Launcher extends JFrame{
 	public Launcher(){
 		setTitle("HarryPotter");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setExtendedState(JFrame.MAXIMIZED_BOTH);
+		setVisible(true);
+	}
+	
+	public void addPanel(JPanel p){
+		this.add(p);
 	}
 
 }
