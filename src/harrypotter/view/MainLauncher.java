9b0fb966a653617b8b21208c894356209e801934
GPhantom <shadytantawy@protonmail.com>
2017-04-17 02:05:41 +0200

    Edited MainLauncher to add the NewGameButton
    - Plus removed the old buttons (Play , Quit).

--- src/harrypotter/view/MainLauncher.java
+++ src/harrypotter/view/MainLauncher.java
@@ -14,7 +14,7 @@ import javax.swing.JPanel;
 
 public class MainLauncher extends Launcher{
 
-	JPanel btnPanel ;
+	//JPanel btnPanel ;
 	
 	public MainLauncher() throws IOException{
 		super();
@@ -25,14 +25,16 @@ public class MainLauncher extends Launcher{
 	            g.drawImage(image, 0, 0,null);
 	        }
 	    });
-	    btnPanel = new JPanel(new BorderLayout());
-	    add(btnPanel);
+	    //btnPanel = new JPanel(new BorderLayout());
+	    //add(btnPanel);
 	}
 	
 	public void addButtons(ArrayList <JButton> btns)
 	{ // Play , Quit	
-		btnPanel.add(btns.get(0),BorderLayout.WEST);
-		btnPanel.add(btns.get(1),BorderLayout.EAST);		
+		//btnPanel.add(btns.get(0),BorderLayout.WEST);
+		//btnPanel.add(btns.get(1),BorderLayout.EAST);
+		//btnPanel.add(btns.get(2),BorderLayout.SOUTH);
+		this.add(btns.get(0));
 	}
 
 }
