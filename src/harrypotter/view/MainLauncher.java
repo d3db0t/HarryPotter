6fa3e573ad6cac2a3da54d1add2a71cdfbbdde77
ThePinger <omaremadsabry@gmail.com>
2017-04-19 23:14:54 +0200

    Updated the addButtons() in MainLauncher

--- src/harrypotter/view/MainLauncher.java
+++ src/harrypotter/view/MainLauncher.java
@@ -4,6 +4,8 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.io.IOException;
 import java.util.ArrayList;
+
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -14,11 +16,34 @@ public class MainLauncher extends JPanel{
 		setLayout(null); // To be able to change the locations of objects inside the panel
 	}
 	
-	public void addButtons(ArrayList <JButton> btns)
+	public void addButtons(JButton btn)
 	{	
-		this.add(btns.get(0));
-		this.add(btns.get(1));
-		this.add(btns.get(2));
+		String name = btn.getName();
+		switch(name)
+		{
+			case "DragonButton":
+				ImageIcon dragonintroimg = (new ImageIcon("DragonIntro.gif"));
+				btn.setIcon(dragonintroimg);
+				btn.setBounds(300,10, dragonintroimg.getIconWidth(), dragonintroimg.getIconHeight());
+				btn.setBorder(BorderFactory.createEmptyBorder());
+				btn.setContentAreaFilled(false);
+				break;
+			case "HarryPotterLogo":
+				ImageIcon harrypotterlogoimg = (new ImageIcon("HPlogo.png"));
+				btn.setIcon(harrypotterlogoimg);
+				btn.setBounds(20,40, harrypotterlogoimg.getIconWidth(), harrypotterlogoimg.getIconHeight());
+				btn.setBorder(BorderFactory.createEmptyBorder());
+				btn.setContentAreaFilled(false);
+				break;
+			case "NewGameButton":
+				ImageIcon newgameimg = (new ImageIcon("NewGameButton.png"));
+				btn.setIcon(newgameimg);
+				btn.setBounds(900,450, newgameimg.getIconWidth(), newgameimg.getIconHeight());
+			    btn.setBorder(BorderFactory.createEmptyBorder());
+				btn.setContentAreaFilled(false);
+				break;
+		}
+		this.add(btn);
 	}
 	
 	public void paintComponent(Graphics g){
