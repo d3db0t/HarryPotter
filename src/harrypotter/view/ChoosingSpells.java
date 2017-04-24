22a25d8552a08cd3d415158626904bfce38ca83e
GPhantom <shadytantawy@protonmail.com>
2017-04-24 19:31:31 +0200

    Added addHouseImage() method in ChoosingSpells
    -Plus added addButtons() method to add buttons.
    - This method adds a pic of the school at the top left corner in the ChoosingSpells view (Panel).

--- src/harrypotter/view/ChoosingSpells.java
+++ src/harrypotter/view/ChoosingSpells.java
@@ -1,6 +1,7 @@
 package harrypotter.view;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.GridLayout;
@@ -9,7 +10,10 @@ import java.awt.Image;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import harrypotter.model.character.Wizard;
 
@@ -18,7 +22,7 @@ public class ChoosingSpells extends JPanel
 	private JPanel champInfo;
 	private JPanel spells;
 	
-    public ChoosingSpells()
+    public ChoosingSpells(String school)
     {
     	setLayout(null);
         this.champInfo = new JPanel();
@@ -30,11 +34,52 @@ public class ChoosingSpells extends JPanel
         this.spells.setBounds(100, 50, 450, 450);
         //add(this.champInfo , BorderLayout.NORTH);
         add(this.spells);
+        addHouseImage(school);
     }
     
     public void addSpellButton(JButton btn)
     {
     	this.spells.add(btn);
+    }
+    
+    public void addButtons(JButton btn){
+    	this.add(btn);
+    }
+    
+    public void addHouseImage(String s){
+    	JButton btn = new JButton();
+    	switch(s){
+    	case "RavenClawButton":
+			ImageIcon ravenclawimg = (new ImageIcon("r.png"));
+            btn.setIcon(ravenclawimg);
+            btn.setBounds(50,40, ravenclawimg.getIconWidth(), ravenclawimg.getIconHeight());
+    		btn.setBorder(BorderFactory.createEmptyBorder());
+    		btn.setContentAreaFilled(false);
+    		break;
+		case "HufflepuffButton":
+			ImageIcon hufflepuffimg = (new ImageIcon("h.png"));
+			btn.setIcon(hufflepuffimg);
+			btn.setBounds(50,40, hufflepuffimg.getIconWidth(), hufflepuffimg.getIconHeight());
+			btn.setBorder(BorderFactory.createEmptyBorder());
+			btn.setContentAreaFilled(false);
+			break;
+		case "GryffindorButton":
+			ImageIcon gryffindorimg = (new ImageIcon("g.png"));
+            btn.setIcon(gryffindorimg);
+			btn.setBounds(50,40, gryffindorimg.getIconWidth(), gryffindorimg.getIconHeight());
+			btn.setBorder(BorderFactory.createEmptyBorder());
+			btn.setContentAreaFilled(false);
+			break;
+		case "SlytherinButton":
+			ImageIcon slytherinimg = (new ImageIcon("s.png"));
+			btn.setIcon(slytherinimg);
+			btn.setBounds(50,40, slytherinimg.getIconWidth(), slytherinimg.getIconHeight());
+			btn.setBorder(BorderFactory.createEmptyBorder());
+			btn.setContentAreaFilled(false);
+			break;
+    	
+    	}
+    	addButtons(btn);
     }
    
     public void paintComponent(Graphics g){
