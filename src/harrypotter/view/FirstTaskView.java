7a63740c669b2907da35c3c29842710d3a2cdbc2
GPhantom <shadytantawy@protonmail.com>
2017-04-25 19:58:48 +0200

    [+] Added FirstTaskView

---
+++ src/harrypotter/view/FirstTaskView.java
@@ -0,0 +1,12 @@
+package harrypotter.view;
+
+import javax.swing.ImageIcon;
+
+public class FirstTaskView extends TaskView{
+	
+	public FirstTaskView(){
+		super();
+		super.getMap()[4][4].setIcon(new ImageIcon("/home/gphantom/Downloads/HarryPotterAssets/egg.png"));
+	}
+
+}
