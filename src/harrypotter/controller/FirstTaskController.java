9cf7c0ba7e5ca2963b0393c1a3d9d3b3955dc2e4
ThePinger <omaremadsabry@gmail.com>
2017-04-25 22:22:12 +0200

    [+] Added Generate map in FirstTaskController

--- src/harrypotter/controller/FirstTaskController.java
+++ src/harrypotter/controller/FirstTaskController.java
@@ -1,27 +1,42 @@
 package harrypotter.controller;
 
+import java.awt.Color;
 import java.io.IOException;
 import java.util.ArrayList;
+
+import javax.swing.JButton;
 
 import harrypotter.exceptions.OutOfBordersException;
 import harrypotter.model.character.Champion;
 import harrypotter.model.tournament.FirstTask;
 import harrypotter.model.tournament.Tournament;
+import harrypotter.model.world.Cell;
+import harrypotter.model.world.ChampionCell;
+import harrypotter.model.world.CollectibleCell;
+import harrypotter.model.world.CupCell;
+import harrypotter.model.world.ObstacleCell;
+import harrypotter.model.world.WallCell;
 import harrypotter.view.FirstTaskView;
 import harrypotter.view.Launcher;
 
-public class FirstTaskController extends TaskController{
+public class FirstTaskController {
 
 	private FirstTask firstTask;
 	private FirstTaskView firstTaskView;
-	
+	private Tournament tournament;
+	private Launcher launcher;
+
 	public FirstTaskController(Launcher launcher, ArrayList <Champion> champs , Tournament tournament)
 	{
-		super(launcher, champs , tournament);
+		this.launcher = launcher;
+		this.tournament = tournament;
+		this.tournament.getChampions().addAll(champs);
+		firstTaskView = new FirstTaskView();
+		this.launcher.add(firstTaskView);
 		try {
-			super.getTournament().beginTournament();
-			this.firstTask = super.getTournament().getFirstTask();
-			
+			this.tournament.beginTournament();
+			this.firstTask = this.tournament.getFirstTask();
+			generateMap();
 		} catch (OutOfBordersException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -29,7 +44,36 @@ public class FirstTaskController extends TaskController{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		firstTaskView = new FirstTaskView();
-		super.getLauncher().add(firstTaskView);
+		
+	}
+	
+	public void generateMap()
+	{
+		Cell [][] map = firstTask.getMap();
+		int c = 0;
+		for(int i = 0 ; i < 10 ; i++)
+		{
+			for(int j = 0 ; j < 10 ; j++)
+			{
+				Cell a = map[i][j]; 
+				if(a instanceof ChampionCell)
+				{
+				   JButton btn = this.firstTaskView.getButtonsMap()[i][j];
+				   btn.setIcon(this.firstTaskView.getGifs().get(c));
+				   c++; 
+				}
+				else if (a instanceof ObstacleCell)
+				{
+				   JButton btn = this.firstTaskView.getButtonsMap()[i][j];
+				   btn.setText("Dragon");
+				   btn.setForeground(Color.WHITE);
+				}
+				else if (i == 4 && j == 4)
+				{
+				   JButton btn = this.firstTaskView.getButtonsMap()[i][j];
+                   btn.setIcon(this.firstTaskView.getGifs().get(4));
+				}
+			}
+		}
 	}
 }
