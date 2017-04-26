d62779c9f04c107e647e3b1ca9af6cbd647c3e76
ThePinger <omaremadsabry@gmail.com>
2017-04-26 16:37:30 +0200

    Removed comments from interface TaskActionListener

--- src/harrypotter/controller/TaskActionListener.java
+++ src/harrypotter/controller/TaskActionListener.java
@@ -3,9 +3,9 @@ package harrypotter.controller;
 public interface TaskActionListener {
 
 	public void moveUp();
-	//public void moveDown();
-	//public void moveRight();
-	//public void moveLeft();
+	public void moveDown();
+	public void moveRight();
+	public void moveLeft();
 	//public void removeChamp();
 	//public void removeObstacle();
 	//public void activateTrait();
