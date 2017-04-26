2baeaedd13510113519178287159bdd09ecfd8db
ThePinger <omaremadsabry@gmail.com>
2017-04-26 14:38:21 +0200

    [+] Added TaskActionListener and TournamentListener

---
+++ src/harrypotter/controller/TaskActionListener.java
@@ -0,0 +1,15 @@
+package harrypotter.controller;
+
+public interface TaskActionListener {
+
+	public void moveUp();
+	//public void moveDown();
+	//public void moveRight();
+	//public void moveLeft();
+	//public void removeChamp();
+	//public void removeObstacle();
+	//public void activateTrait();
+	//public void damagingSpell();
+	//public void healingSpell();
+	//public void relocatingSpell();
+}
---
+++ src/harrypotter/controller/TournamentListener.java
@@ -0,0 +1,9 @@
+package harrypotter.controller;
+
+public interface TournamentListener {
+
+	//public void gameOver();
+	//public void startSecondTask();
+	//public void startThirdTask();
+	//public void showWinner();
+}
