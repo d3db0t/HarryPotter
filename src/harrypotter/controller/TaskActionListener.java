9b7a1e79c20bd39718c40bfaa2de1f4041cb33c2
ThePinger <omaremadsabry@gmail.com>
2017-04-29 14:11:05 +0200

    Added some methods in TaskActionListener
    -These to be called by model in case of traits

--- src/harrypotter/controller/TaskActionListener.java
+++ src/harrypotter/controller/TaskActionListener.java
@@ -1,9 +1,12 @@
 package harrypotter.controller;
 
+import java.awt.Point;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import harrypotter.exceptions.OutOfBordersException;
 import harrypotter.model.magic.Spell;
+import harrypotter.model.world.Direction;
 
 public interface TaskActionListener {
 
@@ -15,6 +18,9 @@ public interface TaskActionListener {
 	public void showFire();
 	public void updatePotions();
 	public void castHealing();
+	public void moveSlytherin(Direction d);
+	public void showMarkedCells(ArrayList <Point> p);
+	public void moveGryffindorOnTrait();
 	//public void removeChamp();
 	//public void removeObstacle();
 	//public void activateTrait();
