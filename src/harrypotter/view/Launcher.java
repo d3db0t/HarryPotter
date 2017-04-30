c04622f3ecfebb44a09dcd7ea2683d2ba3d72ef0
ThePinger <omaremadsabry@gmail.com>
2017-04-30 06:24:27 +0200

    Added part in launcher to notify user on close

--- src/harrypotter/view/Launcher.java
+++ src/harrypotter/view/Launcher.java
@@ -1,18 +1,66 @@
 package harrypotter.view;
 
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class Launcher extends JFrame{
+public class Launcher extends JFrame implements WindowListener{
 	public Launcher(){
 		setTitle("HarryPotter");
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setExtendedState(JFrame.MAXIMIZED_BOTH);
 		setVisible(true);
+	    this.addWindowListener(this);
 	}
 	
 	public void addPanel(JPanel p){
 		this.add(p);
+	}
+
+	@Override
+	public void windowOpened(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(this, "Are you sure you want to exit ? all game progress will be over");
+		System.exit(0);
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowActivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
